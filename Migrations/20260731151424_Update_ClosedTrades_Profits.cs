b2a2b0b8c648ec8e956126c93620339268f7743a
umarmirza24 <umermirza17@gmail.com>
2026-07-31 20:18:23 +0500

    profit table update

---
+++ Migrations/20260731151424_Update_ClosedTrades_Profits.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace finsight_dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_ClosedTrades_Profits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""FSClosedTrades""
+                SET ""NetProfit"" = ((s.""TradePrice"" - b.""TradePrice"") * b.""Quantity"") - (b.""Commission"" + s.""Commission"")
+                FROM ""FSTrades"" b, ""FSTrades"" s
+                WHERE ""FSClosedTrades"".""OrderOpenId"" = b.""ExternalId""
+                  AND ""FSClosedTrades"".""OrderCloseId"" = s.""ExternalId"";
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
